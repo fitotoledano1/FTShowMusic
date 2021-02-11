de7594f2dfd486d0fc457c85566f6d939dcd2130
Fito Toledano <fito.toledano@gmail.com>
2021-02-11 15:14:46 +0100

    Initial Commit

---
+++ FTShowMusic/FTShowMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FTShowMusicApp.swift
+//  FTShowMusic
+//
+//  Created by Fito Toledano on 2/11/21.
+//
+
+import SwiftUI
+
+@main
+struct FTShowMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
